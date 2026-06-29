3f355449794fc7ab84dda772c73d3e4a44c51df4
Tuan Tran <721750440+tuantran@users.noreply.github.com>
2026-06-29 06:03:26 +0000

    Fix header copyrights
    Add missing Meta copyright headers to 4 Python files:
    - videoseal/data/__init__.py
    - videoseal/evals/__init__.py
    - videoseal/losses/perceptual.py
    - videoseal/models/wam.py

--- videoseal/models/wam.py
+++ videoseal/models/wam.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 """
 Test with:
     python -m videoseal.models.wam
